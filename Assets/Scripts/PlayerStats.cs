008df467e0e2abda403cf1d2ba6fa584c3d30794
dkdumpling <79526410+emilisbridzius@users.noreply.github.com>
2023-12-14 12:18:07 +0000

    more stuff

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -8,9 +8,58 @@ public class PlayerStats : MonoBehaviour
     public float money;
     public float score;
     public float kills;
-    public Range turrets = new(0, 4);
-    public Range shields = new(0, 8);
+    public float turrets;
+    public float shields;
     public float baseDamage;
     public float baseHealth;
     public float baseShieldHealth;
+
+    private void Start()
+    {
+        baseDamage = 10;
+        baseHealth = 5;
+        baseShieldHealth = 5;
+        shields = 1;
+    }
+
+    void AddDamage()
+    {
+        if (money >= 50)
+        {
+            baseDamage += 10;
+            money -= 50;
+        }
+    }
+
+    void AddHealth()
+    {
+        if (money >= 50)
+        {
+            baseHealth += 1;
+            money -= 50;
+        }
+    }
+
+    void AddShieldHealth()
+    {
+
+    }
+
+    void AddTurret()
+    {
+        if (turrets < 4 && money >= 200)
+        {
+            turrets += 1;
+            money -= 200;
+        }
+    }
+
+    void AddShield()
+    {
+        if (shields < 8 && money >= 100)
+        {
+            shields += 1;
+            money -= 100;
+        }
+    }
 }
