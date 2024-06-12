f419ebe87a112ee541adb90054d9fd58c67505eb
emilisbridzius <79526410+emilisbridzius@users.noreply.github.com>
2024-06-12 21:05:47 +0100

    some mobile settings
    Attempted UI fixes, added input system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -107,7 +107,7 @@ public class Enemy : MonoBehaviour
                 if (!retreating)
                 {
                     transform.position += pathway * movementSpeed * Time.deltaTime;
-                   
+                    //transform.rotation = Quaternion.LookRotation(Vector2.right, playerLocation.position);
                 }
                 
 
